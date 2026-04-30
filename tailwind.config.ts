430b6fea267f90b4884d1be2229f70d2b8fc1617
kaledmolina <kaledmoly@gmail.com>
2026-04-30 14:54:16 -0500

    feat: add custom font family configurations to tailwind theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -56,7 +56,12 @@ const config: Config = {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		fontFamily: {
+  			sans: ['var(--font-sans)', 'sans-serif'],
+  			mono: ['var(--font-mono)', 'monospace'],
   		}
+
   	}
   },
   plugins: [tailwindcssAnimate],
